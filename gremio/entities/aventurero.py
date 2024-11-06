59da93e3e56969afd6380cef56a1056ac173494c
martinUM8052 <mvignolo@correo.um.edu.uy>
2024-11-06 14:23:49 -0300

    Update aventurero.py
    Agrego clase abstracta y calculo el rango con la formula matematica 
    Rango = habilidad / 20 +1

--- gremio/entities/aventurero.py
+++ gremio/entities/aventurero.py
@@ -1,1 +1,22 @@
 #clase abstracta, metodo calcular rango abstracto. 
+from abc import ABC, abstractmethod
+class Aventurero(ABC):
+    def __init__(self, habilidad):
+        self.habilidad = habilidad
+
+    @abstractmethod
+    def calcular_rango(self):
+        """Calcula el rango del aventurero."""
+        pass
+
+class Guerrero(Aventurero):
+    def calcular_rango(self):
+        return self.habilidad // 20 + 1
+
+# INPUT y validar que ingrese un numero
+try:
+    habilidad = int(input("Ingrese la habilidad del guerrero: "))
+    guerrero = Guerrero(habilidad=habilidad)
+    print(f"Rango del guerrero: {guerrero.calcular_rango()}")
+except ValueError:
+    print("Ingrese un número válido para la habilidad.")
