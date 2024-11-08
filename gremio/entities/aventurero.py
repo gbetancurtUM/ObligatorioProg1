85b946d55816a11ff0d25c0133926df55ddedde5
gbetancurtUM <gbetancurt@correo.um.edu.uy>
2024-11-07 22:01:45 -0300

    Correcciones

--- gremio/entities/aventurero.py
+++ gremio/entities/aventurero.py
@@ -1,22 +1,37 @@
-#clase abstracta, metodo calcular rango abstracto. 
 from abc import ABC, abstractmethod
+from entities.mision import Mision
+
 class Aventurero(ABC):
-    def __init__(self, habilidad):
-        self.habilidad = habilidad
+    def __init__(self, nombre: str, id: int, puntos_habilidad: int, experiencia: int, dinero: float):
+        self.nombre = nombre
+        self.id = id
+        self.puntos_habilidad = puntos_habilidad
+        self.experiencia = experiencia
+        self.dinero = dinero
+        self.misiones = [] 
 
     @abstractmethod
-    def calcular_rango(self):
-        """Calcula el rango del aventurero."""
+    def calcular_rango(self)-> int:
         pass
 
-class Guerrero(Aventurero):
-    def calcular_rango(self):
-        return self.habilidad // 20 + 1
+    def _calcular_rango_base(self, habilidad_total: int) -> int:
+        if habilidad_total <= 20:
+            return 1
+        elif habilidad_total <= 40:
+            return 2
+        elif habilidad_total <= 60:
+            return 3
+        elif habilidad_total <= 80:
+            return 4
+        else:
+            return 5
 
-# INPUT y validar que ingrese un numero
-try:
-    habilidad = int(input("Ingrese la habilidad del guerrero: "))
-    guerrero = Guerrero(habilidad=habilidad)
-    print(f"Rango del guerrero: {guerrero.calcular_rango()}")
-except ValueError:
-    print("Ingrese un número válido para la habilidad.")
+    def calcular_rango(self) -> int:
+        habilidad_total = self.calcular_habilidad_total()
+        return self._calcular_rango_base(habilidad_total)
+
+    def incrementar_experiencia(self, puntos: int):
+        self.experiencia += puntos
+
+    def agregar_mision(self, mision: Mision):
+        self.misiones.append(mision)
